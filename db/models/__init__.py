d455c0ccb7073232fe57375bf440475652a9e0dd
AshishKingdom <ashishkushwahacb@gmail.com>
2024-06-07 17:12:30 +0530

    added user_documents table

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,2 +1,3 @@
 from db.models.customer import Customer
 from db.models.db_config import DBConfig
+from db.models.user_document import UserDocument
---
+++ db/models/user_document.py
@@ -0,0 +1,18 @@
+from db import Base
+from sqlalchemy import Column, Integer, String, TIMESTAMP, UUID, ForeignKey
+from uuid import uuid4
+from datetime import datetime
+
+
+class UserDocument(Base):
+    __tablename__ = 'user_documents'
+
+    id = Column(Integer, primary_key=True, index=True)
+    customer_uuid = Column(UUID(as_uuid=True), ForeignKey(
+        'customers.uuid'), nullable=False)
+    document_name = Column(String, nullable=False)
+    document_url = Column(String, nullable=False)
+    embed_url = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=datetime.utcnow)
+    updated_at = Column(TIMESTAMP, nullable=False,
+                        default=datetime.utcnow, onupdate=datetime.utcnow)
