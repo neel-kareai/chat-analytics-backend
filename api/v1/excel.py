ff34a52e855e235670e3bea6236754135a33ca89
AshishKingdom <ashishkushwahacb@gmail.com>
2024-06-17 04:54:48 +0530

    added crud for excel

--- api/v1/excel.py
+++ api/v1/excel.py
@@ -13,12 +13,12 @@ from logger import logger
 from db import get_db
 from db.queries.user_documents import UserDocumentQuery
 from db.queries.chat_history import ChatHistoryQuery
-from schemas.user_documents import UserDocumentUploadResponse
+from schemas.user_documents import UserDocumentUploadResponse, UserDocumentUpdateRequest
 from sqlalchemy.orm import Session
 from config import Config
 import random
 from helper.openai import create_document_embedding
-
+import os
 
 router = APIRouter(prefix="/excel", tags=["excel"])
 
@@ -74,4 +74,134 @@ async def upload_csv(
             document_name=new_excel_doc.document_name,
             document_type=new_excel_doc.document_type,
         ),
+    )
+
+
+@router.get("/{document_id}")
+async def get_excel_document(
+    document_id: int,
+    response: Response,
+    current_user: AccessTokenData = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> APIResponseBase:
+    """
+    Get the Excel document by its ID.
+
+    Args:
+        document_id (int): The ID of the document to get.
+        response (Response): The response object to be returned.
+        current_user (AccessTokenData, optional): The current user's access token data. Defaults to Depends(get_current_user).
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        APIResponseBase: The API response containing the document data.
+    """
+    excel_doc = UserDocumentQuery.get_user_document_by_id(db, document_id, "excel")
+    if not excel_doc:
+        logger.error("Excel document not found")
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return APIResponseBase.not_found(message="Excel document not found")
+
+    if str(excel_doc.customer_uuid) != current_user.uuid:
+        logger.error("Unauthorized access")
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return APIResponseBase.unauthorized(message="Unauthorized access")
+
+    return APIResponseBase.success_response(
+        message="Excel document found",
+        data=UserDocumentUploadResponse(
+            document_id=excel_doc.id,
+            document_name=excel_doc.document_name,
+            document_type=excel_doc.document_type,
+        ),
+    )
+
+
+@router.put("/{document_id}")
+async def update_excel_document(
+    document_id: int,
+    request: UserDocumentUpdateRequest,
+    response: Response,
+    current_user: AccessTokenData = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> APIResponseBase:
+    """
+    Update the Excel document by its ID.
+
+    Args:
+        document_id (int): The ID of the document to update.
+        request (UserDocumentUpdateRequest): The request object containing the new document details.
+        response (Response): The response object to be returned.
+        current_user (AccessTokenData, optional): The current user's access token data. Defaults to Depends(get_current_user).
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        APIResponseBase: The API response containing the updated document data.
+    """
+    excel_doc = UserDocumentQuery.get_user_document_by_id(db, document_id, "excel")
+    if not excel_doc:
+        logger.error("Excel document not found")
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return APIResponseBase.not_found(message="Excel document not found")
+
+    if str(excel_doc.customer_uuid) != current_user.uuid:
+        logger.error("Unauthorized access")
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return APIResponseBase.unauthorized(message="Unauthorized access")
+
+    excel_doc = UserDocumentQuery.update_user_document(
+        db, document_id, request.document_name
+    )
+
+    db.commit()
+
+    return APIResponseBase.success_response(
+        message="Excel document updated",
+        data=excel_doc.to_dict(),
+    )
+
+
+@router.delete("/{document_id}")
+async def delete_excel_document(
+    document_id: int,
+    response: Response,
+    current_user: AccessTokenData = Depends(get_current_user),
+    db: Session = Depends(get_db),
+) -> APIResponseBase:
+    """
+    Delete the Excel document by its ID.
+
+    Args:
+        document_id (int): The ID of the document to delete.
+        response (Response): The response object to be returned.
+        current_user (AccessTokenData, optional): The current user's access token data. Defaults to Depends(get_current_user).
+        db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Returns:
+        APIResponseBase: The API response containing the status.
+    """
+    excel_doc = UserDocumentQuery.get_user_document_by_id(db, document_id, "excel")
+    if not excel_doc:
+        logger.error("Excel document not found")
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return APIResponseBase.not_found(message="Excel document not found")
+
+    if str(excel_doc.customer_uuid) != current_user.uuid:
+        logger.error("Unauthorized access")
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return APIResponseBase.unauthorized(message="Unauthorized access")
+
+    chat_history = ChatHistoryQuery.get_chat_history(
+        db, current_user.uuid, "excel", excel_doc.id
+    )
+    if chat_history:
+        ChatHistoryQuery.delete_chat_history_by_uuid(db, chat_history.uuid)
+    UserDocumentQuery.delete_user_document(db, document_id)
+    db.commit()
+
+    # Delete the file
+    os.remove(excel_doc.document_url)
+
+    return APIResponseBase.success_response(
+        message="Excel document deleted",
     )
