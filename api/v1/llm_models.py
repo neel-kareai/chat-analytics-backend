a689f9ca57abb2581137dbdfa7c7f79e8161aabf
ashish-kareai <ashish.kushwaha@kareai.io>
2024-06-27 05:02:21 +0530

    added google & anthropic model in in GET /v1/llm_models

--- api/v1/llm_models.py
+++ api/v1/llm_models.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Depends, Header, Response
 from data_response.base_response import APIResponseBase
 from logger import logger
-
+from config import Config
 
 router = APIRouter(prefix="/llm_models", tags=["llm_models"])
 
@@ -17,8 +17,19 @@ async def get_llm_models(response: Response) -> APIResponseBase:
     Returns:
         APIResponseBase: The API response containing the list of LLM models.
     """
-    llm_models = ["gpt-4o", "gpt-4-turbo", "gpt-3.5-turbo-0125"]
     response.status_code = status.HTTP_200_OK
     return APIResponseBase.success_response(
-        message="LLM models fetched successfully", data=llm_models
+        message="LLM models fetched successfully",
+        data={
+            "default_llm_model": Config.DEFAULT_LLM_MODEL,
+            "llm_models": {
+                "openai": ["gpt-4o", "gpt-4-turbo", "gpt-3.5-turbo-0125"],
+                "google": ["gemini-1.5-pro", "gemini-1.5-flash"],
+                "anthropic": [
+                    "claude-3-opus-20240229",
+                    "claude-3-sonnet-20240229",
+                    "claude-3-haiku-20240307",
+                ],
+            },
+        },
     )
